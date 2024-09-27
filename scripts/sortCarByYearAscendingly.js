6b40e08437696bd6734cd8df9d7bb7b54f8444a0
Brandon OddDuckk <Brandon.Ogaden@gmail.com>
2024-09-27 08:43:51 +0800

    feat: Added sortCarByYearAscendingly

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,13 +1,28 @@
 function sortCarByYearAscendingly(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
-
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
+  // Clone array to avoid side effects
   const result = [...cars];
+  
+  /* Variables initialiation */
+  let carsLength = result.length;
+  let swapped, i, j, temp;
 
-  // Tulis code-mu disini
-
-  // Rubah code ini dengan array hasil sorting secara ascending
+  /* for loop for each car in cars */
+  for (i = 0; i < carsLength - 1; i++) {
+    /* Initialize swapped as false */ 
+    swapped = false;
+    /* Inner for loop for swapping */
+    for (j= 0; j < carsLength - i - 1; j++) {
+      if (result[j].year > result[j+1].year) {
+        /* Swap cars if year is greater than the next car's year */
+        temp = result[j];
+        result[j] = result[j+1];
+        result[j+1] = temp;
+        swapped = true;
+      }
+    }
+    if (swapped == false) {
+      break;
+    }
+  }
   return result;
 }
